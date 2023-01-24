53ff89ba6e18a49eca8602ea965298de036824e7
JsonLucas <jasonlucas443@gmail.com>
2023-01-23 21:46:59 -0300

    movie filters

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,15 @@ import { Header } from "../../components/Header";
 import { Movie } from "../../components/Movie";
 import { IMovie, movies } from "../../types/movie";
 import { IoMdList } from "react-icons/io";
-import { Box, Select, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 export function Home() {
   const [filter, setFilter] = useState<string>();
   const [filteredMovies, setFilteredMovies] = useState<Array<IMovie>>();
+  const { getFavorites } = useLocalStorage();
   const applyFilter = ({ target }: any) => {
     const filter = target.value;
     if (filter !== "0") {
@@ -19,17 +21,41 @@ export function Home() {
     if (filter) {
       switch (filter) {
         case "year":
-		  const sortByYear = movies.sort((a, b) => new Date(a.year).getTime() - new Date(b.year).getTime());
-		  setFilteredMovies(sortByYear);
+          const sortByYear = movies.sort(
+            (a, b) => new Date(a.year).getTime() - new Date(b.year).getTime()
+          );
+          setFilteredMovies(sortByYear);
           break;
         case "name":
-		  const sortByName = movies.sort();
-		  setFilteredMovies(sortByName);
+          const sortByName = movies.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            else return 1;
+          });
+          setFilteredMovies(sortByName);
           break;
         case "country":
-		  const sortByCountry = movies.sort();
-		  setFilteredMovies(sortByCountry);
+          const sortByCountry = movies.sort((a, b) => {
+            if (a.countryName < b.countryName) return -1;
+            else return 1;
+          });
+          setFilteredMovies(sortByCountry);
           break;
+		case 'favorites':
+			const favorites = getFavorites();
+			if(favorites) {
+				const favoriteMovies: any = favorites.find((item) => {
+					let array: Array<IMovie> = [];
+					for(let i of movies){
+						if(i.id === item){
+							array.push(i);
+						}
+					}
+					return array;
+				});
+				console.log(favoriteMovies);
+				setFilteredMovies([]);
+			}
+			break;
         default:
           toast("Invalid filter.", { type: "error", theme: "dark" });
           break;
@@ -42,11 +68,12 @@ export function Home() {
       <Box mt="15px" ml="4%" w="90%">
         <Box pb="10px" display="flex">
           <IoMdList color="black" size={25} />
-          <select style={{ border: 'none', marginLeft: '10px' }} onChange={applyFilter}>
+          <select style={{ border: "none", marginLeft: "10px" }} onChange={applyFilter}>
             <option value="0">Filtrar por: </option>
             <option value="year">Ano</option>
             <option value="name">Nome</option>
             <option value="country">País</option>
+            <option value="favorites" disabled={getFavorites().length === 0 ? true : false}>Favoritos</option>
           </select>
         </Box>
         {filter && filteredMovies && (
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -11,65 +11,65 @@ export const movies: Array<IMovie> = [
 	{
 		id: 1,
 		name: 'Star Wars III: A Vingança dos Sith',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-12-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'George Lucas',
+		countryName: 'EUA',
+		year: new Date('2005-05-20'),
+		image: 'https://upload.wikimedia.org/wikipedia/pt/5/58/Star_Wars_Epis%C3%B3dio_III_A_Vingan%C3%A7a_dos_Sith.jpg'
 	},
 	{
 		id: 2,
 		name: 'Piratas do Caribe: No Fim do Mundo',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-09-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'Gore Verbinski',
+		countryName: 'EUA',
+		year: new Date('2007-05-26'),
+		image: 'https://br.web.img3.acsta.net/pictures/210/176/21017697_20130704202238456.jpg'
 	},
 	{
 		id: 3,
 		name: 'Guardiões da Galáxia',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-01-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'James Gunn',
+		countryName: 'EUA',
+		year: new Date('2014-07-31'),
+		image: 'https://br.web.img3.acsta.net/pictures/14/06/03/21/11/122582.jpg'
 	},
 	{
 		id: 4,
 		name: 'Matrix Reloaded',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-05-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'Wachowski Brothers',
+		countryName: 'EUA',
+		year: new Date('2003-05-17'),
+		image: 'https://lastfm.freetls.fastly.net/i/u/500x500/1d16e8760bbb42c1894e8fdc2414779d.jpg'
 	},
 	{
 		id: 5,
-		name: '47 Ronin',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-02-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		name: '47 Ronins',
+		directorName: 'Carl Rinsch',
+		countryName: 'EUA',
+		year: new Date('2014-01-31'),
+		image: 'https://m.media-amazon.com/images/I/81OKIu77+6L._AC_SY550_.jpg'
 	},
 	{
 		id: 6,
 		name: 'Missão Impossível: Efeito Fallout',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-11-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'Christopher McQuarrie',
+		countryName: 'EUA',
+		year: new Date('2018-07-27'),
+		image: 'https://br.web.img3.acsta.net/pictures/18/05/14/17/09/5117345.jpg'
 	},
 	{
 		id: 7,
 		name: 'Batman: O Cavaleiro das Trevas',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-08-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		directorName: 'Christopher Nolan',
+		countryName: 'EUA/Inglaterra',
+		year: new Date('2008-07-19'),
+		image: 'https://upload.wikimedia.org/wikipedia/pt/d/d1/The_Dark_Knight.jpg'
 	},
 	{
 		id: 8,
-		name: 'Invasores: Nenhum sistema Está a Salvo',
-		directorName: 'director name',
-		countryName: 'country name',
-		year: new Date('2022-07-23'),
-		image: 'https://t.ctcdn.com.br/T1R3OGdR8BomakG4ZcOaYY1zDBY=/1400x788/smart/i256255.jpeg'
+		name: 'Invasores: Nenhum sistema Está à Salvo',
+		directorName: 'Baran bo Odar',
+		countryName: 'Alemanha',
+		year: new Date('2014-09-26'),
+		image: 'https://br.web.img3.acsta.net/c_310_420/pictures/15/07/21/20/26/495789.jpg'
 	},
 ];
